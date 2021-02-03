232a16bb0ac24141b55552520106ae61d3925db6
swhachey <sw.hachey@gmail.com>
2021-02-03 14:03:49 -0700

    added event listeners to new recipe buttons and console.logging parts of the objects

--- script.js
+++ script.js
@@ -3,42 +3,38 @@ event.preventDefault();
 
 var apikey = "3c48ac9f4fb24f0da8619831bed373c0";
         var recipeInput = $("#recipeinput").val();
-        var queryURL = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + apikey + "&query=" + recipeInput + "&addRecipeNutrition=true&sort=calories&sortDirection=asc&number=10";
+        var queryURL = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + apikey + "&query=" + recipeInput + "&addRecipeNutrition=true&number=10";
         console.log(recipeInput);
+        //&sort=calories&sortDirection=asc
        
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response){
-            // var recipeCalories = response.results[0].nutrition.nutrients[0].amount;
-            // var recipeCaloriesPerc = response.results[0].nutrition.nutrients[0].percentOfDailyNeeds;
-            // var recipeIMG = response.results[0].image;
-            // var recipeName = response.results[0].title;
-            // var recipeURL = response.results[0].sourceUrl;
-            // var recipeIngredients = JSON.stringify(response.results[0].nutrition.ingredients);
-            // var recipeResults = response.results[0];
-
-        
-            
-            
-            //dynamically create buttons for each recipe to choose from
+          $("#recipeinfo").empty();
+        //dynamically create buttons for each recipe to choose from
             for (var i = 0; i < 5; i++) {
-var recipeCalories = response.results[i].nutrition.nutrients[i].amount;
+            var recipeCalories = response.results[i].nutrition.nutrients[i].amount;
             var recipeCaloriesPerc = response.results[i].nutrition.nutrients[i].percentOfDailyNeeds;
             var recipeIMG = response.results[i].image;
             var recipeName = response.results[i].title;
             var recipeURL = response.results[i].sourceUrl;
             var recipeIngredients = JSON.stringify(response.results[i].nutrition.ingredients);
             var recipeResults = response.results[i];
-            console.log(response);
-            console.log(recipeCalories, recipeIMG, recipeName, recipeURL, recipeCaloriesPerc,);
+            // console.log(response);
+            
 
-                
-            $("<buttons>", {text: recipeName + "   -   " + recipeCalories + "Cal"}).addClass("button is-primary").appendTo("#recipeinfo");
+              //once they choose a recipe, dynamically create the nutirition info, recipes steps, and ingredients    
+            $("<button>", {text: recipeName + "   -   " + recipeCalories + "Cal"}).addClass("button is-danger recipebuttons").appendTo("#recipeinfo");
             }
-            //once they choose a recipe, dynamically create the nutirition info, recipes steps, and ingredients 
+
+            $(".recipebuttons").on("click", function(){
+            console.log(this)
+            console.log(recipeCalories, recipeIMG, recipeName, recipeURL, recipeCaloriesPerc,);
+            });
+           
             //dynamically append that info to the DOM
-            //
+            
         }) //ajax closers
 
     }) //recipeBtn click closers
